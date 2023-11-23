1531482377f0f768d12509decbfc3a21370b713a
kamaldeep-sidhu <kamaldeepkaur2@academic.rrc.ca>
2023-11-23 12:16:37 -0600

    this step imcludes defining of frequency Accessor/ Mutator test cases: Set up project directory and file structure

--- sdf_assignment_6/assignment_6_starter/mortgage/mortgage.py
+++ sdf_assignment_6/assignment_6_starter/mortgage/mortgage.py
@@ -8,100 +8,100 @@ calculate payments.
 from mortgage.pixell_lookup import MortgageRate, MortgageFrequency, VALID_AMORTIZATION
 
 class Mortgage:
-    def __init__(self, loan_amount: float, rate: MortgageRate, frequency: MortgageFrequency, amortization: int):
-        # Validate Loan Amount
-        if loan_amount > 0:
-            self.loan_amount = loan_amount
-        else:
-            raise ValueError("Loan Amount must be positive.")
+        def __init__(self, loan_amount: float, rate: MortgageRate, frequency: MortgageFrequency, amortization: int):
+            # Validate Loan Amount
+            if loan_amount > 0:
+                self.loan_amount = loan_amount
+            else:
+                raise ValueError("Loan Amount must be positive.")
 
-        # Validate Rate
-        try:
-            self.rate = MortgageRate(rate)
-        except ValueError:
-            raise ValueError("Invalid Rate provided.")    
+            # Validate Rate
+            try:
+                self.rate = MortgageRate(rate)
+            except ValueError:
+                raise ValueError("Invalid Rate provided.")    
 
-        # Validate Frequency
-        if not isinstance(frequency, MortgageFrequency):
-            raise ValueError("Invalid Mortgage Frequency provided.")
+            # Validate Frequency
+            if not isinstance(frequency, MortgageFrequency):
+                raise ValueError("Invalid Mortgage Frequency provided.")
 
-        self.frequency = frequency
+            self.frequency = frequency
 
-        # Validate Amortization
-        if amortization in  VALID_AMORTIZATION:
-            self.amortization = amortization
-        else:
-            raise ValueError("Invalid Amortization period provided.")
+            # Validate Amortization
+            if amortization in  VALID_AMORTIZATION:
+                self.amortization = amortization
+            else:
+                raise ValueError("Invalid Amortization period provided.")
+            
+
+            # Initialize other attributes...
+            self._amount = None  # Private attribute to store Loan Amount
+            self.set_amount(loan_amount)  # Use the mutator to set the Loan Amount
+
+        # Mutator  for Loan Amount
+        def set_amount(self, value):
+            if value > 0:
+                self._amount = value
+            else:
+                raise ValueError("Loan Amount must be positive.")
+
+        # Accessor  for Loan Amount
+        def get_amount(self):
+            return self._amount
         
 
-        # Initialize other attributes...
-        self._amount = None  # Private attribute to store Loan Amount
-        self.set_amount(loan_amount)  # Use the mutator to set the Loan Amount
 
-    # Mutator  for Loan Amount
-    def set_amount(self, value):
-        if value > 0:
-            self._amount = value
-        else:
-            raise ValueError("Loan Amount must be positive.")
+            # Initialize other attributes...
+            self._rate = None  # Private attribute to store Rate
+            self.set_rate(rate)  # Use the mutator to set the Rate
 
-    # Accessor  for Loan Amount
-    def get_amount(self):
-        return self._amount
+        # Mutator (Setter) for Rate
+        def set_rate(self, value):
+            if not isinstance(value, MortgageRate):
+                raise ValueError("Invalid Rate provided. Must be an instance of MortgageRate enum.")
+            self._rate = value
+
+        # Accessor (Getter) for Rate
+        def get_rate(self):
+            return self._rate
+        
+
+        # def __init__(self, frequency):
+        #     self._frequency = frequency  # Private attribute with an underscore
+
+        # Accessor (Getter)
+        def get_frequency(self):
+            return self._frequency
+
+        # Mutator (Setter)
+        def set_frequency(self, new_frequency):
+            if not isinstance(new_frequency, MortgageFrequency):
+                raise ValueError("Invalid frequency provided")
+            self._frequency = new_frequency
+        
+        # def __init__(self, frequency, amortization):
+        #     # ... other attributes and initialization code
+        #     self._frequency = frequency
+        #     self._amortization = amortization
+
+        
+    
     
 
 
-        # Initialize other attributes...
-        self._rate = None  # Private attribute to store Rate
-        self.set_rate(rate)  # Use the mutator to set the Rate
-
-    # Mutator (Setter) for Rate
-    def set_rate(self, value):
-        if not isinstance(value, MortgageRate):
-            raise ValueError("Invalid Rate provided. Must be an instance of MortgageRate enum.")
-        self._rate = value
-
-    # Accessor (Getter) for Rate
-    def get_rate(self):
-        return self._rate
-    
-
-    # def __init__(self, frequency):
-    #     self._frequency = frequency  # Private attribute with an underscore
-
-    # Accessor (Getter)
-    def get_frequency(self):
-        return self._frequency
-
-    # Mutator (Setter)
-    def set_frequency(self, new_frequency):
-        if not isinstance(new_frequency, MortgageFrequency):
-            raise ValueError("Invalid frequency provided")
-        self._frequency = new_frequency
-    
-    # def __init__(self, frequency, amortization):
-    #     # ... other attributes and initialization code
-    #     self._frequency = frequency
-    #     self._amortization = amortization
-
-    
-
-   
-
-
-    
-
-
-
-
-
-
-
-        
         
 
-        
 
-        
+
+
+
+
+
+            
+            
+
+            
+
+            
 
 
